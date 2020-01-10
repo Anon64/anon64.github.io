ee37f57d498151245a10f3de94c4d339bf00a25f
Anon64 <Anon64@users.noreply.github.com>
2020-01-10 06:40:21 -0500

    updates
    just things

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('nothing here yet');
+function loadList() {
+    let list;
+    let xmlhttp;
+    
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', "names.txt", true);
+    xmlhttp.send();
+    list = xmlhttp.responseText.split('\r\n');
+
+    document.getElementById('tracknum').innerHTML = `Showing ${list.length} tracks.`;
+}
