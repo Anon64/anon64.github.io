3618ae045e4857bc31735f59441aec333f8115bc
Anon64 <Anon64@users.noreply.github.com>
2020-01-11 12:50:54 -0500

    Update main.js
    change

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-async function loadList() {
+function loadList() {
     let list;
     let xmlhttp;
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.open('GET', "/misc/names.txt");
     xmlhttp.send();
-    /*xmlhttp.onreadystatechange = () => {
-
-    }*/
-    list = await xmlhttp.responseText.split('\n').filter(Boolean);
-
-    document.getElementById('tracknum').innerHTML = `Showing ${list.length} track(s).`;
-    console.log(list);
+    xmlhttp.onload = () => {
+        list = xmlhttp.responseText.split('\n').filter(Boolean);
+        document.getElementById('tracknum').innerHTML = `Showing ${list.length} track(s).`;
+        console.log(list);
+    }
 }
