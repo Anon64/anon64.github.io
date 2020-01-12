d850ea3c9f7f09d02b258f296149bc219652dad9
Anon64 <Anon64@users.noreply.github.com>
2020-01-12 16:10:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ function wait(ms) {
 
 function loadList() {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', "/misc/names.txt");
+    xmlhttp.open('GET', "./misc/names.txt");
     xmlhttp.send();
     xmlhttp.onload = async () => {
         let list = xmlhttp.responseText.split('\n').filter(Boolean);
         let table = document.getElementById('mainlist');
-        for (name of list) {
+        for (let [id, name] of list.entries()) {
             document.getElementById('tracknum').innerHTML = `Showing ${list.indexOf(name)+1} track(s).`;
             let row = table.insertRow(-1);
-            row.insertCell(0).innerHTML = list.indexOf(name);
+            row.insertCell(0).innerHTML = id;
             row.insertCell(1).innerHTML = name;
             await wait(30);
         }
