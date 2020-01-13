315100c33a509fa8368b65179d4cf39ff628932f
Anon64 <Anon64@users.noreply.github.com>
2020-01-12 23:20:51 -0500

    add search
    stuff here

--- js/main.js
+++ js/main.js
@@ -18,7 +18,23 @@ function onLoad() {
     }
 }
 
+function bettersort(q) {
+    return function (a, b) {
+        return a.indexOf(q) - b.indexOf(q);
+    }
+}
+
+function searchlist(q = '') {
+    return list.filter(n => {
+        n.toLowerCase().includes(q) && !!v
+    }).sort(bettersort(q)).entries();
+}
+
+let busy = false;
+
 async function loadList() {
+    if (busy) return;
+    busy = true;
     let table = document.getElementById('mainlist');
 
     let idval = document.getElementById('startindex').value;
@@ -26,27 +42,32 @@ async function loadList() {
 
     let length = document.getElementById('amount').value || 20;
 
+    let search = document.getElementById('searchbox').value || '';
+
     let summatory = 0;
     if (table.rows.length > 1) {
         let rowCount = table.rows.length;
         for (let i = 1; i < rowCount; i++) {
             table.deleteRow(1);
             document.getElementById('tracknum').innerHTML = `Showing ${table.rows.length-1} track(s) of ${list.length}.`;
-            let newtime = (2000 - summatory) / ((1.05 - Math.pow(1.05, -(rowCount - 2))) / (1.05 - 1));
+            let newtime = (500 - summatory) / ((1.05 - Math.pow(1.05, -(rowCount - 2))) / (1.05 - 1));
             summatory += newtime;
-            await wait(newtime);
+            if (newtime > 10) await wait(newtime);
         }
     }
 
     summatory = 0;
-    let listlength = list.slice(index, index + length).length;
-    for (let [id, name] of list.slice(index, index + length).entries()) {
+    let songlist = search ? searchlist(search) : list.slice(index, index + length).entries();
+    let listlength = songlist.length;
+    for (let [id, name] of songlist) {
         let row = table.insertRow(-1);
         row.insertCell(0).innerHTML = id + index;
         row.insertCell(1).innerHTML = name;
         document.getElementById('tracknum').innerHTML = `Showing ${table.rows.length-1} track(s) of ${list.length}.`;
-        let newtime = (2000 - summatory) / ((1.05 - Math.pow(1.05, -(listlength - 1))) / (1.05 - 1));
+        let newtime = (500 - summatory) / ((1.05 - Math.pow(1.05, -(listlength - 1))) / (1.05 - 1));
         summatory += newtime;
-        await wait(newtime);
+        if (newtime > 10) await wait(newtime);
     }
+    await wait(200);
+    busy = false;
 }
