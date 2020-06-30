4152334ef647baa460c1debe6ba736b933b216e7
Anon64 <Anon64@users.noreply.github.com>
2020-06-29 20:19:11 -0400

    more updates

--- 404/404.js
+++ 404/404.js
@@ -1,10 +1,16 @@
-const messages = [
-    'bruh moment, you made an oopsie',
-    'you aren\'t supposed to see this',
+const messages = [ 
+    'bruh moment, you made an oopsie.',
+    'you aren\'t supposed to see this.',
     'what did you do???? (or was it me?)',
-    'yikes, ignore this'
+    'yikes, ignore this.',
+    'could you try that again, please?',
+    'are you sure you did that right?',
+    'nope, try again.',
+    'whoops, something went wrong.'
 ];
 
-document.onload(() => {
+//bruh what are you doing here, stop snooping around.
+
+function onLoad() {
     document.getElementById('message').innerHTML = `404<br>${messages[Math.floor(messages.length * Math.random())] || 'oops'}`;
-})
+}
