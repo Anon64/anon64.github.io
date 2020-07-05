e06a071e979e1355a87f114f5b42774a4ce31ab5
Anon64 <Anon64@users.noreply.github.com>
2020-07-05 14:13:46 -0400

    revert change (until can figure out ssl)

--- mpp2/js/script.js
+++ mpp2/js/script.js
@@ -1106,7 +1106,7 @@ $(function () {
 	if (channel_id == "") channel_id = "lobby";
 
 	var wssport = window.location.hostname == "www.multiplayerpiano.com" ? 443 : 8080;
-	var gClient = new Client('wss://headless-mbp.gq:2269');//new Client("ws://" + window.location.hostname + ":" + wssport);
+	var gClient = new Client('ws://headless-mbp.gq:2269'); //new Client("ws://" + window.location.hostname + ":" + wssport);
 	gClient.setChannel(channel_id);
 	gClient.start();
 
