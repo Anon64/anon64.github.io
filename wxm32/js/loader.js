d0b96f94a15013e1f6127a4b21efd42488dbc3e7
Anon64 <30940003+Anon64@users.noreply.github.com>
2022-09-05 04:39:17 -0400

    Update loader.js
    last try bruh

--- wxm32/js/loader.js
+++ wxm32/js/loader.js
@@ -20,9 +20,9 @@ function getList() {
     document.getElementById('wxm').innerHTML = "WXM32 & WCGQ-FM (107.3) - <span style='color: #FFFF90;'>Connecting to alert server.</span>";
 
     try {
-        getJSON("https://acikek.com/alert").then(list => {
+        getJSON("https://acikek.com/alert").then(l => {
             //document.getElementById('wxm').innerHTML = `WXM32 & WCGQ-FM (107.3) - <span style='color: #FF9090;'>Error parsing alerts.</span>`;
-            list = list.alerts.sort((a, b) => b.date - a.date);
+            list = l.alerts.sort((a, b) => b.date - a.date);
             document.getElementById('wxm').innerHTML = `WXM32 & WCGQ-FM (107.3) - <span style='color: #90FF90;'>Collected ${list.length} alert${list.length == 1 ? '' : 's'}.</span>`;
             retry_count = 0;
             loadList();
